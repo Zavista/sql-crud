e158633197ca5e13e0be013250a68fc6678b1192
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-11 01:20:03 -0500

    Added an AddUser control

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -11,3 +11,19 @@ export const getUsers = (req, res) => {
             res.status(200).json(result);
     })
 }
+
+
+//ADDs a user
+export const addUser = (req, res) => {
+    let { name, email, phone, birthdate } = req.body;
+    const sql = `INSERT INTO users('name', 'email', 'phone', 'birthdate') VALUES(${name}, ${email}, ${phone}, ${birthdate})`;
+
+    db.query(sql, (err) => {
+        if (err) {
+            res.status(500).json({message: err.message});
+        }
+        res.status(200).json("User has been successfully created.");
+    })
+
+
+}
