3b01d1b276ac951edbf7e750aaf7d315c7dcdb9b
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-10 19:33:40 -0500

    Add userRoutes and set connection to db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import usersRouter from './src/routes/users.js'
+import { db } from './src/config/db.js';
+
 
 const app = express();
 
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    
+    console.log("MySQL connected");
+})
+
+
 app.use(express.json());
 app.use(cors());
+
+
+// Routes
+app.use('/', usersRouter);
 
 app.listen(5000);
