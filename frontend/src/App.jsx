d113a799d74c402f3f99667b103042a58c8cdc9d
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-10 20:00:23 -0500

    Created Container div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2`
+  
+`
+
 
 function App() {
 
   return (
     <>
-      
+      <Container>
+        Hi
+      </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTEM_LEFT}></ToastContainer>
     </>
       
