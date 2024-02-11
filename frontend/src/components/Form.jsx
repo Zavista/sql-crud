87da0e856a92f26fc046ed4dc7c6ad4ef715265c
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-10 20:06:02 -0500

    SetUp Form component

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+
+`
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
