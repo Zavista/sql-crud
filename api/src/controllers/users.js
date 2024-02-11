11c6be22258a4b3dc4daf82879cdd6867037f2c9
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-11 01:34:51 -0500

    Created update and deleteUser controls

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -15,15 +15,47 @@ export const getUsers = (req, res) => {
 
 //ADDs a user
 export const addUser = (req, res) => {
-    let { name, email, phone, birthdate } = req.body;
-    const sql = `INSERT INTO users('name', 'email', 'phone', 'birthdate') VALUES(${name}, ${email}, ${phone}, ${birthdate})`;
+    const q =
+      "INSERT INTO users(`name`, `email`, `phone`, `birthdate`) VALUES(?)";
+  
+    const values = [
+      req.body.name,
+      req.body.email,
+      req.body.phone,
+      req.body.birthdate,
+    ];
+  
+    db.query(q, [values], (err) => {
+      if (err) return res.json(err);
+  
+      res.status(200).json("User has been successfully added.");
+    });
+  };
 
-    db.query(sql, (err) => {
-        if (err) {
-            res.status(500).json({message: err.message});
-        }
-        res.status(200).json("User has been successfully created.");
+//UPDATEs a user
+export const updateUser = (req, res) => {
+    const sql = "UPDATE users SET `name` = ?, `email` = ?, `phone` = ?, `birthdate` = ? WHERE `id` = ?";
+
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.birthdate,
+      ];
+    
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+    
+        res.status(200).json("User has been successfully updated.");
+    });
+}
+
+//DELETEs a user
+export const deleteUser = (req, res) => {
+    const sql = "DELETE FROM users where `id` = ?";
+    db.query(q, [req.params.id], (err) => {
+        if (err) return res.json(err);
+    
+        res.status(200).json("User has been successfully deleted.");
     })
-
-
 }
