1e71f99c2945c619d77efd7b5b4d119c47b1bcf1
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-02-11 00:30:12 -0500

    Created a form with inputs

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
 
 const FormContainer = styled.form`
@@ -17,10 +17,51 @@ const InputArea = styled.div`
   display: flex;
   flex-direction: column;
 `
+const Label = styled.label`
+  
+`
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`
 
-const Form = () => {
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+`
+
+
+const Form = ({ onEdit }) => {
+  const ref = useRef();
+  
   return (
-    <div>Form</div>
+    <FormContainer>
+      <InputArea>
+        <Label for="name">Name</Label>
+        <Input name="name" id="name"></Input>
+      </InputArea>
+      <InputArea>
+        <Label for="email">Email</Label>
+        <Input name="email" id="email"></Input>
+      </InputArea>
+      <InputArea>
+        <Label for="tel">Telephone</Label>
+        <Input name="tel" id="tel"></Input>
+      </InputArea>
+      <InputArea>
+        <Label for="birthdate">Date of Birth</Label>
+        <Input name="birthdate" id="birthdate" type='date'></Input>
+      </InputArea>
+      <Button type='submit'>Submit</Button>
+    </FormContainer>
   )
 }
 
